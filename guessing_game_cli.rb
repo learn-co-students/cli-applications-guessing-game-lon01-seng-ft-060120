0665d3feb2479182b667934c3cf1bfe80e75e4cc
Daniel Andrew <dwandrew@hotmail.com>
2020-04-08 10:12:48 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,7 @@
 # Code your solution here!
 def run_guessing_game 
-num = rand(6) +1
+num = rand(6)+1 
+puts "Enter a number between 1 and 6."
 user_num = gets
 if user_num == 'exit'
   puts "Goodbye!"
